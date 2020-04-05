ab15a79f681da0f979e2d90502dfeb8081556b45
bigoceans <1647383460@qq.com>
2020-04-05 14:17:49 +0800

    Site updated: 2020-04-05 14:17:48

--- js/botui.js
+++ js/botui.js
@@ -59,7 +59,7 @@ function bot_ui_ini() {
         end = function () {
             botui.message.add({
                 delay: 600,
-                content: "![...](https://view.moezx.cc/images/2018/05/06/a1c4cd0452528b572af37952489372b6.md.jpg)"
+                content: "![...](https://cdn.jsdelivr.net/gh/bigoceans/cdn/img/aboutme.png)"
             })
         },
         secondpart = function () {
